27d8115c022137e2c8a0297262ed58ccbd0deb0c
Lucas Tassi <bytechlucas@gmail.com>
2026-05-11 13:13:45 -0300

    fix express duplicate

--- server.js
+++ server.js
@@ -1,33 +1,4 @@
 const express = require('express')
-const cors = require('cors')
-
-const app = express()
-
-app.use(cors())
-
-app.get('/script.js', (req, res) => {
-
-    res.setHeader(
-        'Content-Type',
-        'application/javascript'
-    )
-
-    res.send(`
-
-        console.log('Node conectado na Nuvemshop')
-
-        document.body.style.background = '#111'
-
-    `)
-
-})
-
-app.listen(3000, () => {
-
-    console.log('Servidor rodando')
-
-})
-const express = require('express')
 
 const app = express()
 
